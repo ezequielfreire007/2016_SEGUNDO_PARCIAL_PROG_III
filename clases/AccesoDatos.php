b63f1806ee28f0d4623722c812e032dd3bd50f7c
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-11-07 23:21:51 -0300

    modificacion en AccesoDatos.php

--- clases/AccesoDatos.php
+++ clases/AccesoDatos.php
@@ -1,29 +1,40 @@
 <?php
 class AccesoDatos
 {
+    //Atributos
+    private static $_objAccesoDatos;
+    private $_objPDO;
 		//IMPLEMENTAR...
     private function __construct()
     {
 		//IMPLEMENTAR...
+        try {
+            $this->_objPDO = new PDO('mysql:host=localhost;dbname=login_pdo;charset=utf8','root','123',array(PDO::ATTR_EMULATE_PREPARES=>false,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            $this->_objPDO-exec("SET CHARACTER SET utf8");
+        } catch (PDOException $e) {
+            echo "Error".'<br>'.$e->getMessage();
+            die();
+        }
+
     }
- 
+
     public function RetornarConsulta($sql)
-    { 
+    {
 		//IMPLEMENTAR...
     }
-    
+
      public function RetornarUltimoIdInsertado()
-    { 
+    {
 		//IMPLEMENTAR...
     }
- 
+
     public static function dameUnObjetoAcceso()
-    { 
+    {
 		//IMPLEMENTAR...
     }
- 
+
     public function __clone()
-    { 
+    {
  		//IMPLEMENTAR...
     }
 }
