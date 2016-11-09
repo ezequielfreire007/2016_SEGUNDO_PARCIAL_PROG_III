d6bf756bf3b9bc3f66ad342ac7de6c21fb1aa2b6
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-11-08 22:04:46 -0300

    se completa el metodo TraerTodosLosPerfiles de Usuario

--- clases/Usuario.php
+++ clases/Usuario.php
@@ -1,14 +1,23 @@
 <?php
+require_once "AccesoDatos.php";
 
 class Usuario {
 
-//--CONSTRUCTOR
+    //Atributos
+    private $id;
+    private $nombre;
+    private $email;
+    private $password;
+    private $perfil;
+    private $foto;
+
+    //--CONSTRUCTOR
     public function __construct($id = NULL) {
         if ($id !== NULL) {
-		//IMPLEMENTAR...
+            $this->id = $id;
         }
     }
-    
+
     public static function TraerUsuarioLogueado($obj) {
 		//IMPLEMENTAR...
     }
@@ -30,11 +39,21 @@ class Usuario {
     }
 
     public static function TraerTodosLosUsuarios() {
-		//IMPLEMENTAR...
+
+
     }
 
     public static function TraerTodosLosPerfiles() {
-		//IMPLEMENTAR...
+        //Instancio un objeto a acceso a datos
+        $objetoAccesoDatos = AccesoDatos::dameUnObjetoAcceso();
+        //Genero la consulta
+        $consulta = $objetoAccesoDatos->RetornarConsulta("SELECT `perfil` FROM `usuarios`");
+        //Executo la consulta
+        $consulta->execute();
+        //Genero un array de los perfiles
+        $perfiles = $consulta->fetchObjet('array');
+        //retorno el array de perfiles
+        return $perfiles;
     }
 
     public static function Borrar($id) {
